b5cdf3c8d3440191a69c810218f2a9bd0656632e
Mario Mattia Di Muro <mattia.digitalmosaik@gmail.com>
2025-07-06 18:21:38 +0200

    feat(db): setup aurora database and typeorm

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10) || 5432,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [], // Aggiungeremo le entità qui
+        synchronize: true, // Attenzione: usare solo in sviluppo!
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
