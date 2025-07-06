d582eb80040c1986ab8b9661cb6d9fa454bab58a
Mario Mattia Di Muro <mattia.digitalmosaik@gmail.com>
2025-07-06 16:46:43 +0200

    feat: Add force_delete to ECR repositories

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -4,6 +4,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "dydat-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -14,6 +15,7 @@ resource "aws_ecr_repository" "backend" {
 resource "aws_ecr_repository" "frontend" {
   name                 = "dydat-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
