21af498f755ee70b5cca7efbcbdedba5d50de9d3
Villa Puricelli <agente.villapuricelli@gmail.com>
2026-04-08 23:33:45 +0200

    avvia-metodo-villa.bat: cap 50 sub-blocchi per la catena B39
    Alzato --max-blocks da 13 a 50 per permettere al runner di completare
    l'intera catena B39 (38 sub-blocchi) in un singolo doppio click del
    .bat. Resta il timeout 45 min per blocco + verbose.

--- avvia-metodo-villa.bat
+++ avvia-metodo-villa.bat
@@ -13,7 +13,7 @@ if defined GITBASH (
     echo Trovato Git Bash: %GITBASH%
     echo Avvio runner...
     echo.
-    "%GITBASH%" -c "cd '%~dp0' && chmod +x metodo-villa-runner.sh && ./metodo-villa-runner.sh --max-blocks 13 --timeout 45 --verbose"
+    "%GITBASH%" -c "cd '%~dp0' && chmod +x metodo-villa-runner.sh && ./metodo-villa-runner.sh --max-blocks 50 --timeout 45 --verbose"
     echo.
     echo === Runner terminato ===
     pause
